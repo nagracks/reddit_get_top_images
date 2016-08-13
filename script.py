78d3fd363850946bb5fe5376f8ce9dfe43974c82
nagracks <nagracks@users.noreply.github.com>
2016-08-13 06:49:08 +0530

    Add author builder script

---
+++ script.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__    = "nagracks"
+__date__      = "13-08-2016"
+__license__   = "GPL3"
+__copyright__ = "Copyright © 2016 nagracks"
+
+import os
+import requests
+
+header = """\
+Lead by:
+* (nagracks)[http://github.com/nagracks]
+
+"""
+url = "https://api.github.com/repos/nagracks/reddit_get_top_images/contributors"
+
+r = requests.get(url)
+contributors = r.json()
+
+lines = []
+for i in contributors:
+    url = (i.get('html_url'))
+    username = i.get('login')
+    lines.append('* ({})[{}] '.format(username, url))
+
+text = header + '\n'.join(lines)
+
+with open('AUTHORS.md', 'w') as f:
+    f.write(text)
