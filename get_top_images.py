31b30c91c8a11f009b25c256392f7bc9e7c66d58
nagracks <nagracks@users.noreply.github.com>
2017-03-25 19:17:32 +0530

    Fix trailing whitespace

--- get_top_images.py
+++ get_top_images.py
@@ -251,15 +251,15 @@ def _parse_args():
     """
     parser = argparse.ArgumentParser(description="Download top pics from any subreddit")
     parser.add_argument(
-            '--subreddit', '-s', 
-            default=['earthporn', 'cityporn'], 
-            nargs='+', 
+            '--subreddit', '-s',
+            default=['earthporn', 'cityporn'],
+            nargs='+',
             help="""
                 Name of the subreddits. Provide as many subreddits you
                 want to download images from.
             """
             )
-    parser.add_argument('--period', '-p', 
+    parser.add_argument('--period', '-p',
             default='w',
             choices=['h', 'd', 'w', 'm', 'y', 'a'],
             help="""
@@ -269,16 +269,16 @@ def _parse_args():
             """
             )
     parser.add_argument(
-            '--limit', '-l', 
-            metavar='N', 
+            '--limit', '-l',
+            metavar='N',
             type=int,
-            default=15, 
+            default=15,
             help="""
                 Maximum URL limit per subreddit. Defaults to 15
             """
             )
     parser.add_argument(
-            '--destination', '-d', 
+            '--destination', '-d',
             dest='dst',
             help="""
                 Destination path. By default it saves to $HOME/reddit_pics
