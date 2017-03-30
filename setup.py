3aa35b0e8995850fd87eaef8b100e72b70fd57b5
nagracks <nagracks@users.noreply.github.com>
2017-03-30 09:34:45 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name='reddit_get_top_images',
+    version='v0.1.0',
+    description='Download top images from any subreddit',
+    author='nagracks',
+    author_email='nagracks@gmail.com',
+    url='https://github.com/nagracks/reddit_get_top_images',
+    download_url='https://github.com/nagracks/reddit_get_top_images/archive/v0.1.0.tar.gz',
+    keywords = ['reddit', 'script', 'download', 'images'],
+    classifiers = [],
+)
