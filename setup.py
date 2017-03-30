ada2a6fc15dcc463827c47f85ac9806c4aaeaee9
nagracks <nagracks@users.noreply.github.com>
2017-03-30 13:14:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
+
+
+with open('requirements.txt') as infile:
+    requirements = infile.read()
 
 
 setup(
-    name='reddit_get_top_images',
     version='v0.1.0',
+    name='reddit_get_top_images',
     description='Download top images from any subreddit',
+    licence='MIT',
+
     author='nagracks',
     author_email='nagracks@gmail.com',
+
     url='https://github.com/nagracks/reddit_get_top_images',
     download_url='https://github.com/nagracks/reddit_get_top_images/archive/v0.1.0.tar.gz',
-    keywords = ['reddit', 'script', 'download', 'images'],
-    classifiers = [],
+
+    keywords=['reddit', 'script', 'download', 'images'],
+
+    scripts=['get_top_images.py'],
+    install_requires=requirements,
 )
